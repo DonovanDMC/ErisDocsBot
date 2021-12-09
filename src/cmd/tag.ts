9e8e5d5fc96ad73f3be5d01dc5cfa648c3ad7940
Donovan Daniels <hewwo@yiff.rocks>
2021-12-09 15:45:09 -0600

    tag command

---
+++ src/cmd/tag.ts
@@ -0,0 +1,51 @@
+import type { ApplicationCommandInteractionDataOptionString } from "../../node_modules/discord-api-types/v9";
+import { InteractionResponseType } from "../../node_modules/discord-api-types/v9";
+import Command from "../util/Command";
+import { log } from "../util/general";
+import EmbedBuilder from "../util/EmbedBuilder";
+
+export default new Command("tag", "Get the content of a specific tag.")
+	.addStringOption("tag", "The tag to get the content of.", [
+		{
+			name: "Version",
+			value: "version"
+		}
+	])
+	.setExecutor(async function (interaction, req, res) {
+		log(interaction, "tag", "command");
+		const tag = (interaction.data.options?.[0] as ApplicationCommandInteractionDataOptionString | undefined)?.value;
+		if (!tag) return;
+
+		switch (tag.toLowerCase()) {
+			case "version": return res.status(200).json({
+				type: InteractionResponseType.ChannelMessageWithSource,
+				data: {
+					embeds: [
+						new EmbedBuilder()
+							.setTitle("Giving Version Information")
+							.setDescription([
+								"If you are asked to provide information about what version of Eris you are using, please do not say \"latest\", \"github\", \"dev\", or anything similar.",
+								"This is essentially meaningless to us as we do not know what you are considering the latest, as you could be running an outdated version."
+							])
+							.toJSON(),
+						new EmbedBuilder()
+							.setTitle("Installed From NPM")
+							.setDescription([
+								"Run the command `npm ls eris` in your project, and give us the number after the \"@\".",
+								"[for example](https://eris.owo-whats-this.dev/eris_version_0161.png), the version in this screenshot is **0.16.1**, on the third line."
+							])
+							.toJSON(),
+						new EmbedBuilder()
+							.setTitle("Installed From Github")
+							.setDescription([
+								"Run the command `npm ls eris` in your project, and give us the commit hash after the \"#\".",
+								"[for example](https://eris.owo-whats-this.dev/eris_version_dev.png), the commit hash in this screenshot is **29a3a635a241a4ab8725c25afa839f29542e1731**, version **0.16.2-dev** (the version number is mostly irrelevant).",
+								"",
+								"If you've installed from a fork ([for instance](https://github.com/DonovanDMC/Eris)) the process above still applies, but make sure to tell us what fork you are using."
+							])
+							.toJSON()
+					]
+				}
+			});
+		}
+	});
