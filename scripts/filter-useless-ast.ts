2260b23b030ef08f71e4a0293726a5981bdbaa36
Donovan Daniels <hewwo@yiff.rocks>
2022-05-15 00:48:57 -0500

    w

--- scripts/filter-useless-ast.ts
+++ scripts/filter-useless-ast.ts
@@ -1,4 +1,4 @@
-import * as fs from "fs";
+import fs from "fs";
 const file = process.argv[2];
 if (!file) throw new Error("missing file");
 const f = JSON.parse(fs.readFileSync(file).toString()) as Array<{
