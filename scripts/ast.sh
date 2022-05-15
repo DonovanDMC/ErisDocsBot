f069263a1a84b82f168fc5f01681bc95b2f6578e
Donovan Daniels <hewwo@yiff.rocks>
2022-05-15 00:43:11 -0500

    fix tsnode calls

--- scripts/ast.sh
+++ scripts/ast.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 SCRIPT_DIR=$(dirname $(readlink -f $0))
-VERSIONS=$(ts-node $SCRIPT_DIR/get-versions.ts)
+VERSIONS=$(node --no-warnings --no-deprecation --experimental-specifier-resolution=node --loader ts-node/esm  $SCRIPT_DIR/get-versions.ts)
 for v in $(echo "$VERSIONS" | jq -r '.[]'); do
 	/usr/bin/env sh -c "$SCRIPT_DIR/ast-run.sh $v $1"
 done
