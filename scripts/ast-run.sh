6e72c8eaa3de94e9bca5c180fae4d556834a7af9
Donovan Daniels <hewwo@yiff.rocks>
2022-05-15 00:38:55 -0500

    bash -> sh

--- scripts/ast-run.sh
+++ scripts/ast-run.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 SCRIPT_DIR=$(dirname $(readlink -f $0))
 
 
--- scripts/ast.sh
+++ scripts/ast.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 SCRIPT_DIR=$(dirname $(readlink -f $0))
 VERSIONS=$(ts-node $SCRIPT_DIR/get-versions.ts)
 for v in $(echo "$VERSIONS" | jq -r '.[]'); do
-	/usr/bin/env bash -c "$SCRIPT_DIR/ast-run.sh $v $1"
+	/usr/bin/env sh -c "$SCRIPT_DIR/ast-run.sh $v $1"
 done
